d23ce74579522bfcc424b0bcf194374637435056
Reuben Frankel <rfrankel@matatika.com>
2025-01-21 11:34:25 +0000

    Prefer workflow matrix approach

--- tests/test_core.py
+++ tests/test_core.py
@@ -5,35 +5,24 @@ from singer_sdk.testing import SuiteConfig, get_tap_test_class
 from tap_msaccess.tap import TapMSAccess
 from tests import custom_tap_tests
 
-SUITE_CONFIG = SuiteConfig(
-    ignore_no_records_for_streams=[
-        "MSysComplexType_UnsignedByte",
-        "MSysComplexType_Short",
-        "MSysComplexType_Long",
-        "MSysComplexType_IEEESingle",
-        "MSysComplexType_IEEEDouble",
-        "MSysComplexType_GUID",
-        "MSysComplexType_Decimal",
-        "MSysComplexType_Text",
-        "MSysComplexType_Attachment",
-    ],
-)
-
+SAMPLE_CONFIG = {}
 
 # Run standard built-in tap tests from the SDK:
 TestTapMSAccess = get_tap_test_class(
     tap_class=TapMSAccess,
-    config={"database_file": "sample_db/Books.accdb"},
+    config=SAMPLE_CONFIG,
     custom_suites=[custom_tap_tests],
-    suite_config=SUITE_CONFIG,
-)
-
-TestTapMSAccessS3 = get_tap_test_class(
-    tap_class=TapMSAccess,
-    config={
-        "database_file": "s3://tap-msaccess/Books.accdb",
-        "anon": True,
-    },
-    custom_suites=[custom_tap_tests],
-    suite_config=SUITE_CONFIG,
+    suite_config=SuiteConfig(
+        ignore_no_records_for_streams=[
+            "MSysComplexType_UnsignedByte",
+            "MSysComplexType_Short",
+            "MSysComplexType_Long",
+            "MSysComplexType_IEEESingle",
+            "MSysComplexType_IEEEDouble",
+            "MSysComplexType_GUID",
+            "MSysComplexType_Decimal",
+            "MSysComplexType_Text",
+            "MSysComplexType_Attachment",
+        ]
+    ),
 )
